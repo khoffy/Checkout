6c249fa81aa3161bdf0839d143aa8f57d1d6a15d
khoffy <khoffy@khoffy>
2023-01-18 09:59:03 -0600

    Creating CheckoutSteps class to implement every step definition

---
+++ step_definitions/CheckoutSteps.java
@@ -0,0 +1,21 @@
+package step_definitions
+public class CheckoutSteps {
+
+    @Given("the price of a {string} is {int} c")
+    public void thePriceOfAIsC(String string, Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I checkout {int} {string}")
+    public void iCheckout(Integer int1, String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the total price should be {int} c")
+    public void theTotalPriceShouldBeC(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
