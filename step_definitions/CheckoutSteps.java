cd5fe152684f196ee226b4d240beec958c03567f
khoffy <khoffy@khoffy>
2023-01-27 04:00:54 -0600

    Adding new java command into .cucumber file in order to compile the file containing the step definitions

--- step_definitions/CheckoutSteps.java
+++ step_definitions/CheckoutSteps.java
@@ -1,21 +1,24 @@
-package step_definitions
+package step_definitions;
+
+import cucumber.api.java.en.*;
+import io.cucumber.java.PendingException;
 public class CheckoutSteps {
 
-    @Given("the price of a {string} is {int} c")
+    @Given("^the price of a \"(.*?)\" is (\\d+) c$")
     public void thePriceOfAIsC(String string, Integer int1) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        throw new PendingException();
     }
 
-    @When("I checkout {int} {string}")
+    @When("^I checkout (\\d+) \"(.*?)\"$")
     public void iCheckout(Integer int1, String string) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        throw new PendingException();
     }
 
-    @Then("the total price should be {int} c")
+    @Then("^the total price should be (\\d+) c$")
     public void theTotalPriceShouldBeC(Integer int1) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        throw new PendingException();
     }
 }
